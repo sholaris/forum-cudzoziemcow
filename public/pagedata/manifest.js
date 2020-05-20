af03eb5a49e4570c98b21a30a14b9aae5cd2d65c
Kamil Stys <kstys8@wp.pl>
2020-05-20 16:35:31 +0200

    Added descriptions for all categories

--- public/pagedata/manifest.js
+++ public/pagedata/manifest.js
@@ -20,3 +20,95 @@ export const postCategories = {
     "Brexit",
   ],
 };
+
+export const categories = [
+  {
+    name: "Karta pobytu",
+    description:
+      "Pytania szeroko związane z dokumentem karty pobytu oferowanej cudzoziemcom w Polsce. Mogą one dotyczyć np. informacji w jaki sposób nabyć nową kartę lub jak przedłużyć już posiadaną.",
+  },
+  {
+    name: "Przedłużenie pobytu",
+    description:
+      "Pytania dotyczące formalności związanych z przedłużeniem pobytu w Polsce a także przedłużeniem ważności wszelkich dokumentów pobytowych.",
+  },
+  {
+    name: "Pobyt stały",
+    description:
+      "Pytania dotyczące wszystkich potrzebnych informacji z zakresu nabycia zezwolenia na pobyt stały w Polsce.",
+  },
+  {
+    name: "Pobyt czasowy",
+    description:
+      "Pytania dotyczące wszystkich potrzebnych informacji z zakresu nabycia zezwolenia na pobyt czasowy w Polsce.",
+  },
+  {
+    name: "Pobyt rezydenta długoterminowego UE",
+    description:
+      "Pytania dotyczące wszystkich potrzebnych informacji z zakresu nabycia zezwolenia na pobyt rezydenta długoterminowego UE.",
+  },
+  {
+    name: "Pobyt obywatela UE/rodziny obywatela UE",
+    description:
+      "Pytania dotyczące wszystkich potrzebnych informacji z zakresu legalizacji pobytu obywatela państwa należącego do Unii Europejskiej w Polsce lub legalizacji pobytu członków jego rodziny.",
+  },
+  {
+    name: "Pobyt tolerowany",
+    description:
+      "Pytania związane z zagadnieniem legalizajci pobyt cudzoziemca w Polsce na podstawie zgody na pobyt tolerowany.",
+  },
+  {
+    name: "Pobyt ze względów humanitarnych",
+    description:
+      "Pytania związane z zagadnieniem legalizajci pobyt cudzoziemca w Polsce na podstawie zgody na pobyt ze względów humanitarnych.",
+  },
+  {
+    name: "Status uchodźcy",
+    description:
+      "Pytania szeroko związane z zagadnieniem legalizacji pobytu cudzoziemca w Polsce na podstawie statusu uchodźcy.",
+  },
+  {
+    name: "Wiza",
+    description: "Pytania związane z dokumentem wizy.",
+  },
+  {
+    name: "Studia w Polsce",
+    description:
+      "Pytania dotyczące informacji potrzebnych cudzoziemcom chcącym podjąć studia na uczelniach wyższych w Polsce lub je kontynuować.",
+  },
+  {
+    name: "Nauka w Polsce",
+    description:
+      "Pytania dotyczące informacji potrzebnych cudzoziemcom chcącym podjąć naukę w Polsce w innej formie niż stacjonarne studia wyższe.",
+  },
+  {
+    name: "Naukowiec w Polsce",
+    description:
+      "Pytania dotyczące informacji potrzebnych cudzoziemcom związanym ze środowiskiem naukowym chcącym podjąć badania naukowe lub rozwojowe w Polsce.",
+  },
+  {
+    name: "Zezwolenie na pracę",
+    description:
+      "Pytania dotyczące informacji potrzebnych cudzoziemcom chcącym legalnie podjąc pracę w Polsce. ",
+  },
+  {
+    name: "Zaproszenie dla cudzoziemca",
+    description:
+      "Pytania dotyczące wszelkich potrzebnych informacji związanych z dokumentem zaproszenia dla cudzoziemca.",
+  },
+  {
+    name: "Przeniesienie wewnątrz przedsiębiorstwa",
+    description:
+      "Pytania docztyczące informacji związanych z legalizacją pobytu cudzoziemca, który został/zostanie przeniesiony przez swoje przedsiębiorstwo.",
+  },
+  {
+    name: "Pobyt obywatela Wielkiej Brytanii",
+    description:
+      "Pytania dotyczące wszelkich informacji potrzebnych cudzoziemcom z Wielkiej Brytanii chcącym legalnie przebywać na terytorium Polski.",
+  },
+  {
+    name: "Brexit",
+    description:
+      "Pytania dotyczące informacji związanych z wyjściem Wielkiej Brytanii z Unii Europejskiej oraz wszelkich innych podobnych tematów. ",
+  },
+];
